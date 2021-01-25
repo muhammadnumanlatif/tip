9f8305806c3c2a3e52b41a50ee4235597caaa25a
Muhammad Numan Latif <muhammadnumanlatif@gmail.com>
2021-01-26 03:30:48 +0500

    Tip Calculator - Statefull Widget

---
+++ lib/ui/home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class TipCalculator extends StatefulWidget {
+  @override
+  _TipCalculatorState createState() => _TipCalculatorState();
+}
+
+class _TipCalculatorState extends State<TipCalculator> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tip Calculator"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[],
+        ),
+      ),
+    );
+  }
+}
